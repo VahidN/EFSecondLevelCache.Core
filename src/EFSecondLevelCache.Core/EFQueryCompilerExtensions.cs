e63e5a1d88d04f5e28f2aef26777d05d1c2174ba
jazea <taoxu.wei@163.com>
2018-03-02 13:54:20 +0800

    ToSql() support Microsoft.EntityFrameworkCore 2.1.0-preview1-final

--- src/EFSecondLevelCache.Core/EFQueryCompilerExtensions.cs
+++ src/EFSecondLevelCache.Core/EFQueryCompilerExtensions.cs
@@ -16,16 +16,20 @@ namespace EFSecondLevelCache.Core
             typeof(QueryCompiler).GetTypeInfo();
         private static readonly FieldInfo _queryCompilerField =
             typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
-        private static readonly PropertyInfo _nodeTypeProviderField =
-            _queryCompilerTypeInfo.DeclaredProperties.Single(x => x.Name == "NodeTypeProvider");
+        private static readonly TypeInfo _gueryModelGeneratorInfo =
+            typeof(QueryModelGenerator).GetTypeInfo();
+        private static readonly FieldInfo _queryModelGeneratorField =
+            _queryCompilerTypeInfo.DeclaredFields.First(x => x.Name == "_queryModelGenerator");
+        private static readonly FieldInfo _nodeTypeProviderField =
+            _gueryModelGeneratorInfo.DeclaredFields.Single(x => x.Name == "_nodeTypeProvider");
         private static readonly MethodInfo _createQueryParserMethod =
-            _queryCompilerTypeInfo.DeclaredMethods.First(x => x.Name == "CreateQueryParser");
+            _gueryModelGeneratorInfo.DeclaredMethods.First(x => x.Name == "CreateQueryParser");
         private static readonly FieldInfo _dataBaseField =
             _queryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
 
-#if NETSTANDARD2_0 || NET4_6_1           
-       private static readonly PropertyInfo _databaseDependenciesProperty = 
-           typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
+#if NETSTANDARD2_0 || NET4_6_1
+        private static readonly PropertyInfo _databaseDependenciesProperty =
+            typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
 #else
         private static readonly FieldInfo _queryCompilationContextFactoryField =
             typeof(Database).GetTypeInfo().DeclaredFields.Single(x => x.Name == "_queryCompilationContextFactory");
@@ -38,14 +42,15 @@ namespace EFSecondLevelCache.Core
         public static string ToSql<TEntity>(this IQueryable<TEntity> query)
         {
             var queryCompiler = (IQueryCompiler)_queryCompilerField.GetValue(query.Provider);
-            var nodeTypeProvider = (INodeTypeProvider)_nodeTypeProviderField.GetValue(queryCompiler);
-            var parser = (IQueryParser)_createQueryParserMethod.Invoke(queryCompiler, new object[] { nodeTypeProvider });
+            var queryModelGenerator = _queryModelGeneratorField.GetValue(queryCompiler);
+            var nodeTypeProvider = (INodeTypeProvider)_nodeTypeProviderField.GetValue(queryModelGenerator);
+            var parser = (IQueryParser)_createQueryParserMethod.Invoke(queryModelGenerator, new object[] { nodeTypeProvider });
             var queryModel = parser.GetParsedQuery(query.Expression);
             var database = _dataBaseField.GetValue(queryCompiler);
 
-#if NETSTANDARD2_0 || NET4_6_1           
-		    var databaseDependencies = (DatabaseDependencies)_databaseDependenciesProperty.GetValue(database); 
-		    var queryCompilationContextFactory = databaseDependencies.QueryCompilationContextFactory;
+#if NETSTANDARD2_0 || NET4_6_1
+            var databaseDependencies = (DatabaseDependencies)_databaseDependenciesProperty.GetValue(database);
+            var queryCompilationContextFactory = databaseDependencies.QueryCompilationContextFactory;
 #else
             var queryCompilationContextFactory = (IQueryCompilationContextFactory)_queryCompilationContextFactoryField.GetValue(database);
 #endif            
